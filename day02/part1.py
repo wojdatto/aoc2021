138a9cd085ab3f6c05880363d039672ce08617b8
Tomasz Wojdat <wojdatto@gmail.com>
2021-12-02 11:20:37 +0100

    Add part1 of day 2

---
+++ day02/part1.py
@@ -0,0 +1,35 @@
+def main():
+    horizontal_pozition = 0
+    depth = 0
+
+    with open("input.txt", "r") as file:
+        lines = []
+        for line in file.readlines():
+            lines.append(line.strip())
+
+    commands = lines
+
+    for cmd in commands:
+
+        instruction, amount = cmd.split()
+        amount = int(amount)
+
+        if instruction == "forward":
+            horizontal_pozition += amount
+        elif instruction == "down":
+            depth += amount
+        elif instruction == "up":
+            depth -= amount
+        else:
+            raise NotImplemented
+
+        print(f"{instruction=}, {amount=}")
+
+    print(f"\n{horizontal_pozition=}, {depth=}")
+
+    print(f"\n{horizontal_pozition*depth=}")
+
+        
+
+if __name__ == "__main__":
+    main()
