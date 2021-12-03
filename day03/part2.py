52ed8bedf2e72c6117985e944bed107699a98809
Tomasz Wojdat <wojdatto@gmail.com>
2021-12-03 20:08:23 +0100

    Add spaghetti solution for part 2 (but it worked!)

--- day03/part2.py
+++ day03/part2.py
@@ -9,48 +9,54 @@ def main(filename):
             bits[idx].append(bit)
 
     oxygen_str = ""
-
-    # previous_more_common = more_common(bits[0])
-    # included_values = bits[0]
     included_values = []
     more_common_indices = [x for x in range(len(bits[0]))]
-    for col_idx, column in enumerate(bits):
+    for column in bits:
         for row_idx, bit in enumerate(column):
             if row_idx in more_common_indices:
                 included_values.append(bit)
 
         oxygen_str += more_common(included_values)
-        # current_more_common = more_common(included_values)
 
         more_common_indices = [i for i, x in enumerate(column) if (x == more_common(included_values) and i in more_common_indices)]
 
         included_values = []
 
-        # # check next column
-        # for index in more_common_indices:
-        #     bits[col_idx+1]
-
-        # included_values: list[str] = []
-        # for bit in column:
-        #     if bit == previous_more_common:
-        #         included_values.append(bit)
-            
-        # oxygen_str += more_common(included_values)
-        # previous_more_common = more_common(column)
-
-
     oxygen = int(oxygen_str, 2)
-
     print(f"{oxygen_str=}")
     print(f"{oxygen=}")
 
-    return oxygen
+
+    co2_str = ""
+    included_values = []
+    less_common_indices = [x for x in range(len(bits[0]))]
+    for column in bits:
+        for row_idx, bit in enumerate(column):
+            if row_idx in less_common_indices:
+                included_values.append(bit)
+
+        if included_values.count("0") == 1:
+            interesting_idx_list = [i for i, x in enumerate(column) if (x == less_common(included_values) and i in less_common_indices)]
+            assert len(interesting_idx_list) == 1
+            co2_str = lines[interesting_idx_list[0]]
+
+        less_common_indices = [i for i, x in enumerate(column) if (x == less_common(included_values) and i in less_common_indices)]
+
+        included_values = []
+
+    co2 = int(co2_str, 2)
+    print(f"{co2_str=}")
+    print(f"{co2=}")
+
+    print(f"{oxygen*co2=}")
+
+    return oxygen * co2
 
 
 def less_common(data: list[str]) -> str:
-    if more_common(data) == "0":
-        return "1"
-    return "0"
+    if data.count("0") <= data.count("1"):
+        return "0"
+    return "1"
 
 
 def more_common(data: list[str]) -> str:
@@ -68,8 +74,8 @@ def _read_file(filename) -> list[str]:
 
 
 def test_main():
-    assert main("day03/example_input.txt") == 23
+    assert main("day03/example_input.txt") == 230
 
 
 if __name__ == "__main__":
-    main("day03/example_input.txt")
+    main("day03/input.txt")
