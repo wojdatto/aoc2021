6f8e19a9178dce8663946bcc200405b2b92b7a59
Tomasz Wojdat <wojdatto@gmail.com>
2021-12-10 13:45:01 +0100

    Add test cases for day 2

--- day02/part2.py
+++ day02/part2.py
@@ -1,21 +1,24 @@
-def main():
-    horizontal_pozition = 0
+INPUT = """\
+forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2
+"""
+
+
+def main(lines: list[str]):
+    horizontal_position = 0
     depth = 0
     aim = 0
 
-    with open("day02/input.txt", "r") as file:
-        lines = []
-        for line in file.readlines():
-            lines.append(line.strip())
-
-    commands = lines
-
-    for cmd in commands:
+    for cmd in lines:
         instruction, amount = cmd.split()
         amount = int(amount)
 
         if instruction == "forward":
-            horizontal_pozition += amount
+            horizontal_position += amount
             depth += aim * amount
         elif instruction == "down":
             aim += amount
@@ -24,12 +27,25 @@ def main():
         else:
             raise AssertionError("We shouldn't end here")
 
-        print(f"{instruction=}\t{amount=}\t{horizontal_pozition=}\t{depth=}\t{aim=}")
+    return horizontal_position * depth
 
-    print(f"\n{horizontal_pozition=}, {depth=}")
 
-    print(f"\n{horizontal_pozition*depth=}")
+def parse_input_file() -> list[str]:
+    with open("day02/input.txt", "r") as file:
+        return file.read().splitlines()
+
+
+def test_main_example_data():
+    assert main(INPUT.splitlines()) == 900
+
+
+def test_main_real_data():
+    assert main(parse_input_file()) == 1857958050
 
 
 if __name__ == "__main__":
-    main()
+    test_result = main(INPUT.splitlines())
+    print(f"{test_result=}")
+
+    real_result = main(parse_input_file())
+    print(f"{real_result=}")
