4514523dd11f47eaa9b84775950e7b8c217f57ee
Tomasz Wojdat <wojdatto@gmail.com>
2021-12-10 20:41:01 +0100

    Use counter in solution for day 6

--- day06/part1.py
+++ day06/part1.py
@@ -1,22 +1,20 @@
+from collections import Counter
+
 INPUT = "3,4,3,1,2"
 DAYS_TO_SIMULATE = 80
 MAX_FISH_AGE = 8
 
 
-def main(input_data, days: int):
-    for _ in range(days):
-        input_data = day_passed(input_data)
-    return len(input_data)
+def main(data: list[int]) -> int:
+    fish = Counter(n for n in data)
 
-
-def day_passed(data: list[int]) -> list[int]:
-    spawned_fish = []
-    for idx, _ in enumerate(data):
-        data[idx] -= 1
-        if data[idx] < 0:
-            data[idx] = 6
-            spawned_fish.append(MAX_FISH_AGE)
-    return data + spawned_fish
+    for _ in range(DAYS_TO_SIMULATE):
+        fish_new = Counter({6: fish[0], 8: fish[0]})
+        for key, val in fish.items():
+            if key > 0:
+                fish_new[key - 1] += val
+        fish = fish_new
+    return sum(fish.values())
 
 
 def parse_input() -> list[int]:
@@ -27,16 +25,16 @@ def parse_input() -> list[int]:
 
 def test_main_example_data():
     test_input = [int(i) for i in INPUT.split(",")]
-    assert main(test_input, DAYS_TO_SIMULATE) == 5934
+    assert main(test_input) == 5934
 
 
 def test_main_real_data():
-    assert main(parse_input(), DAYS_TO_SIMULATE) == 380758
+    assert main(parse_input()) == 380758
 
 
 if __name__ == "__main__":
-    test_result = main([int(i) for i in INPUT.split(",")], DAYS_TO_SIMULATE)
+    test_result = main([int(i) for i in INPUT.split(",")])
     print(f"{test_result=}")
 
-    real_result = main(parse_input(), DAYS_TO_SIMULATE)
+    real_result = main(parse_input())
     print(f"{real_result=}")
--- day06/part2.py
+++ day06/part2.py
@@ -1,32 +1,19 @@
-from collections import defaultdict
-from copy import copy
+from collections import Counter
 
 INPUT = "3,4,3,1,2"
 DAYS_TO_SIMULATE = 256
 MAX_FISH_AGE = 8
 
 
-def main(data: list[int], days: int) -> int:
-    fish = defaultdict(int)
+def main(data: list[int]) -> int:
+    fish = Counter(n for n in data)
 
-    # Initialize the defaultdict with every day
-    for i in range(MAX_FISH_AGE + 1):
-        fish[i]
-
-    for i in data:
-        fish[i] += 1
-
-    for _ in range(days):
-        sum_to_add = copy(fish[0])
-
-        for age in fish:
-            if age == 6:
-                fish[age] = fish[age + 1] + sum_to_add
-            elif age == 8:
-                fish[age] = sum_to_add
-            else:
-                fish[age] = fish[age + 1]
-
+    for _ in range(DAYS_TO_SIMULATE):
+        fish_new = Counter({6: fish[0], 8: fish[0]})
+        for key, val in fish.items():
+            if key > 0:
+                fish_new[key - 1] += val
+        fish = fish_new
     return sum(fish.values())
 
 
@@ -38,16 +25,16 @@ def parse_input() -> list[int]:
 
 def test_main_example_data():
     test_input = [int(i) for i in INPUT.split(",")]
-    assert main(test_input, DAYS_TO_SIMULATE) == 26984457539
+    assert main(test_input) == 26984457539
 
 
 def test_main_real_data():
-    assert main(parse_input(), DAYS_TO_SIMULATE) == 1710623015163
+    assert main(parse_input()) == 1710623015163
 
 
 if __name__ == "__main__":
-    test_result = main([int(i) for i in INPUT.split(",")], DAYS_TO_SIMULATE)
+    test_result = main([int(i) for i in INPUT.split(",")])
     print(f"{test_result=}")
 
-    real_result = main(parse_input(), DAYS_TO_SIMULATE)
+    real_result = main(parse_input())
     print(f"{real_result=}")
