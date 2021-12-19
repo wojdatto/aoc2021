bd6569db2e44f2ee4433ea84b38be7d13352e3e7
Tomasz Wojdat <wojdatto@gmail.com>
2021-12-19 22:55:50 +0100

    Solve part 1 of day 17

---
+++ day17/part1.py
@@ -0,0 +1,77 @@
+from pathlib import Path
+
+INPUT = "target area: x=20..30, y=-10..-5"
+ITERATIONS = 125  # determined empirically :>
+
+
+def main(s: str) -> int:
+    x_min, x_max, y_min, y_max = parse_input_data(s)
+
+    def simulate(vx, vy) -> tuple[int, bool]:
+        def make_step():
+            nonlocal x, y
+            nonlocal vx, vy
+
+            x += vx
+            y += vy
+
+            if vx:
+                vx = vx - 1 if x > 0 else vx + 1
+            vy -= 1
+
+        is_inside = False
+        x, y = 0, 0
+        vx, vy = vx, vy
+
+        max_height = 0
+        steps = 0
+        while x not in range(x_min, x_max + 1) or y not in range(y_min, y_max + 1):
+            make_step()
+            max_height = y if y > max_height else max_height
+            steps += 1
+
+            if x > x_max:
+                break
+            if vx == 0 and y < y_min:
+                break
+        else:
+            is_inside = True
+
+        return max_height, is_inside
+
+    vx, vy = 1, 1
+    max_height = 0
+    for vx in range(ITERATIONS):
+        for vy in range(ITERATIONS):
+            height, is_inside = simulate(vx=vx, vy=vy)
+            if is_inside:
+                max_height = height if height > max_height else max_height
+    return max_height
+
+
+def parse_input_data(s: str) -> tuple[int, int, int, int]:
+    x, y = s.lstrip("target area: ").split(", ")
+    x_min, x_max = x.lstrip("x=").split("..")
+    y_min, y_max = y.lstrip("y=").split("..")
+    return int(x_min), int(x_max), int(y_min), int(y_max)
+
+
+def parse_input_file() -> str:
+    with open(Path(Path(__file__).parent, "input.txt"), "r") as file:
+        return file.read().strip()
+
+
+if __name__ == "__main__":
+    test_result = main(INPUT)
+    print(f"{test_result=}\n")
+
+    real_result = main(parse_input_file())
+    print(f"{real_result=}\n")
+
+
+def test_main_example_data():
+    assert main(INPUT) == 45
+
+
+def test_main_real_data():
+    assert main(parse_input_file()) == 5886
